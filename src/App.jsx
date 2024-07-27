56214165f5d8116912507db1e45af51085e1a2d6
MohamedAliCuEdu <10512020103761.std.ce.edu.eg>
2024-07-28 02:14:42 +0300

    change api url

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
             <Route element={<Layout />}>
               <Route path="/login" element={<Login />}></Route>
               <Route path="/signup" element={<Signup />}></Route>
-                <Route index element={<Home />}></Route>
+              <Route index element={<Home />}></Route>
               <Route element={<PresistLogin />}>
                 <Route element={<AuthRequire />}>
                   <Route path="/profile" element={<Soon />}></Route>
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://organizeit-api.vercel.app";
+const BASE_URL = "https://organizeit-api-production.up.railway.app";
 
 const axiosApi = axios.create({
   baseURL: BASE_URL,
