b5a82bb56ef46ce6c29f77d3921c5ff5ebe95b3e
MohamedAliCuEdu <10512020103761.std.ce.edu.eg>
2024-08-01 21:14:39 +0300

    d

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://organizeit-api.vercel.app";
+const BASE_URL = "http://localhost:4000";
 
 const axiosApi = axios.create({
   baseURL: BASE_URL,
